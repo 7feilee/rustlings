2fc11bc221397ee789a659a4572693a53fd467f6
test <test@tests-MBP.fritz.box>
2023-12-04 18:57:58 +0100

    [+] add solution

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -7,15 +7,16 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input).unwrap_or_else(|err| {
+        println!("Error: {}", err);
+        0
+    });
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,6 +29,7 @@ fn main() {
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
+
     let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -16,7 +14,14 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm... Why is this always returning an Ok value?
+        // Hmm... Why is this always returning an Ok value? Fix this function
+        // so that it checks incoming values and returns the proper result
+        // according to the above enum
+        if value < 0 {
+            return Err(CreationError::Negative);
+        } else if value == 0 {
+            return Err(CreationError::Zero);
+        }
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
